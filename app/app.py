cb025a9bbdb221844e504d3e8cab61c2a9a9f2d1
Girish Vilvankadu Jesudas <girishjesudas@gmail.com>
2025-06-01 00:43:08 +0100

    Updated app/app.py to test CI/CD Pipeline

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD!"
+    return "Hello from Flask CI/CD! you are running your first PIPELINE!!!!!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
