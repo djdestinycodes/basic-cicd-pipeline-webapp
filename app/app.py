c89ef248bbe9a99a58b13a2874791dd74397094c
Girish Vilvankadu Jesudas <girishjesudas@gmail.com>
2025-06-01 01:06:21 +0100

    Upgraded Appy.py to test CI/CD pipepline with Render

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD! you are running your first PIPELINE!!!!!"
+    return "Hello from Flask CI/CD! You are running your First CI/CD Pipeline! Congrats!!!!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
